45435dff4bfbbb573c88020736593457fb5cf4c1
Norsse <raphael.pastre@gmail.com>
2020-01-01 11:50:06 +0100

    remove unused dependecies

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {
